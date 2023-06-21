110179664e283de000f43152ce2a9465332cf960
artemalferyev <art.alferyev@gmail.com>
2023-06-21 21:05:20 +0100

    commit

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -4,19 +4,33 @@ import java.io.InputStreamReader;
 import java.net.*;
 
 public class UDPClient {
-
     public static void main(String[] args) throws IOException {
 
         byte[] receiveBuffer = new byte[1024];
         byte[] sendBuffer = new byte[1024];
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter server IP address: ");
         String host = reader.readLine();
+
         DatagramSocket socket = new DatagramSocket();
-        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, InetAddress.getByName(host), 7777);
+
+        InetAdress serverAddress = InetAddress.getByName(host);
+        int serverPort = 7777;
+
+        String sendData = "Hello, server!";
+        sendBuffer = sendData.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, serverPort);
         socket.send(sendPacket);
+
         DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
         socket.receive(receivePacket);
+
+        String receivedData = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        System.out.println("Received from server: " + receivedData);
+
+
         socket.close();
-        System.out.println(receivePacket);
     }
 }
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -3,21 +3,28 @@ import java.io.IOException;
 import java.net.InetAddress;
 
 public class UDPServer {
-
     public static void main(String[] args) throws IOException {
-
         byte[] sendBuffer = new byte[1024];
         byte[] receiveBuffer = new byte[1024];
         int portNumber = 7777;
+
         DatagramSocket socket = new DatagramSocket(portNumber);
+
         DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
         socket.receive(receivePacket);
-        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, InetAddress.getLocalHost(),portNumber);
+
+        String receivedData = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        System.out.println("Recevied from the client: " + receivedData);
+
+        String sendData = "Hello, client!";
+        sendBuffer = sendData.getBytes();
+
+        InetAddress clientAddress = receivePacket.getAddress();
+        int clientPort = receivePacket.getPort();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress, clientPort);
+
         socket.send(sendPacket);
         socket.close();
-
-        System.out.println(sendBuffer.toString().getBytes());
-        System.out.println(receiveBuffer.toString().getBytes());
-
     }
 }
